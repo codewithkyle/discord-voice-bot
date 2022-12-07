3d5761dc5c878e04a8f0bc82aa16f0d3432431e9
Kyle Andrews <codingwithkyle@gmail.com>
2022-12-06 20:34:04 -0500

    ✨ Switched to discordjs/voice

--- bot.ts
+++ bot.ts
@@ -1,53 +1,64 @@
-const { Client, Message, VoiceState } = require("discord.js");
+const { Client, Message, VoiceState, GatewayIntentBits, Events, Partials } = require("discord.js");
+const { joinVoiceChannel, getVoiceConnection, VoiceConnectionStatus, VoiceRecorder } = require("@discordjs/voice");
 const ffmpeg = require("ffmpeg-static");
 const fs = require("fs");
+const { Readable } = require('stream');
 const record = require("node-record-lpcm16");
 require('dotenv').config();
 
-const client = new Client();
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildVoiceStates],
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+});
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
 client.login(process.env.BOT_TOKEN);
-const usersInChannel = new Map();
 
-async function joinVoiceChannel(message) {
-  const voiceChannels = message.guild.channels.cache.filter(channel => channel.type === "voice");
-  if (voiceChannels.size > 0) {
-    const channel = voiceChannels.first();
-    const connection = await channel.join();
-    client.on("voiceStateUpdate", (oldState, newState) => {
-      if (usersInChannel.has(newState.id) && !newState.channelID) {
-        usersInChannel.delete(newState.id);
-        record.stop(newState.id);
-      }
-      if (!usersInChannel.has(newState.id) && newState.channelID === channel.id) {
-        usersInChannel.set(newState.id, newState);
-        const date = new Date();
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        const audioStream = connection.receiver.createStream(newState.id);
-        const fileStream = fs.createWriteStream(`recording-${year}-${month}-${day}-${newState.id}.wav`);
-        record
-          .start({
-            sampleRateHertz: 48000,
-            threshold: 0,
-            verbose: false,
-            recordProgram: "rec",
-            silence: "10.0",
-          })
-          .pipe(fileStream);
-        audioStream.pipe(record);
-      }
+let voiceRecorder = null;
+
+async function join(message) {
+    const channel = joinVoiceChannel({
+        channelId: message.channelId,
+        guildId: message.guildId,
+        adapterCreator: message.channel.guild.voiceAdapterCreator,
     });
-  }
+    const connection = getVoiceConnection(channel.joinConfig.guildId);
+    connection.on(VoiceConnectionStatus.Ready, async () => {
+        voiceRecorder = new VoiceRecorder(connection, {
+            sampleRate: 48000,
+            channelCount: 2,
+        });
+        voiceRecorder.start();
+    });
 }
 
-client.on("message", (message) => {
-  if (message.content === "join") {
-    joinVoiceChannel(message);
-  }
-  if (message.content === "leave") {
-    const channel = message.member.voice.channel;
-    channel.leave();
-    record.stop();
-  }
+client.on(Events.MessageCreate, async (message) => {
+    if (message.channelId !== "719623619684335767") return;
+    if (message.content === "join") {
+        join(message);
+    }
+    if (message.content === "leave") {
+        const channel = message.member.voice.channel;
+        if (voiceRecorder != null && channel){
+            const audioStream = voiceRecorder.stop();
+            const readableAudioStream = new Readable({
+                read() {
+                    this.push(audioStream);
+                    this.push(null);
+                },
+            });
+            const date = new Date();
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+            channel.members.forEach((member) => {
+                const fileName = `recording-${year}-${month}-${day}-${member.id}.wav`;
+                const wavFile = fs.createWriteStream(fileName);
+                readableAudioStream.pipe(wavFile);
+            });
+        }
+        channel.leave();
+        voiceRecorder = null;
+    }
 });
